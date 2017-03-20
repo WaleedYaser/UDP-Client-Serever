68bb71fd14631729e2816cd3d8701c32231c526b
WaleedYaser <waleedyaser95@gmail.com>
2017-03-21 01:51:02 +0200

    refactor: handle exceptions

--- Project/src/udp/client/server/UDPServer.java
+++ Project/src/udp/client/server/UDPServer.java
@@ -6,12 +6,14 @@ import java.util.logging.Logger;
 
 public class UDPServer {
     
-    public static void main(String[] args) throws IOException {
-           
+    public static void main(String[] args) {
+          DatagramSocket server = null;
         try {
             //  create the udp server socket with specific port number
-            DatagramSocket server=new DatagramSocket(2000);
-           
+            server = new DatagramSocket(2000);
+         } catch (SocketException ex) {
+        Logger.getLogger(UDPServer.class.getName()).log(Level.SEVERE, null, ex);
+      }  
             // preparing the packet structure for recived packet 
           
             byte[]receivedata=new byte[100];
@@ -20,30 +22,34 @@ public class UDPServer {
           
             while (true)
             {
-                // waiting for incoming packet 
-                server.receive(recievepacket);
+                try {
+                    // waiting for incoming packet 
+                    server.receive(recievepacket);
 
-                // Extracting recieved packet 
-                String message =new String (recievepacket.getData());
-                System.out.println("Recieve: " + message);
+                    // Extracting recieved packet 
+                    String message =new String (recievepacket.getData());
+                    System.out.println("Recieve: " + message);
 
-                // send response to the client 
-                System.out.println("Processing ...");
-                String response = ConvertToUpper(message) ;
-                byte[]senddata=response.getBytes();
-                InetAddress IPAddress = InetAddress.getByName("localhost");
-                DatagramPacket sendpacket = new DatagramPacket (senddata, 
-                                                                senddata.length,
-                                                                IPAddress,
-                                                                9090);
-                server.send(sendpacket);
-                System.out.println("Send: " + response);
-                System.out.println("----------------------------");
+                    // send response to the client 
+                    System.out.println("Processing ...");
+                    String response = ConvertToUpper(message) ;
+                    byte[]senddata=response.getBytes();
+                    InetAddress IPAddress = InetAddress.getByName("localhost");
+                    DatagramPacket sendpacket = new DatagramPacket (senddata, 
+                                                                    senddata.length,
+                                                                    IPAddress,
+                                                                    9090);
+                    server.send(sendpacket);
+                    System.out.println("Send: " + response);
+                    System.out.println("----------------------------");
+                    } catch (NullPointerException ex) {
+                        System.err.println("Null exception");
+                    } catch (IOException ex) {
+                        System.err.println("IO exception");
+                    }
             } 
-                  
-        } catch (SocketException ex) {
-        Logger.getLogger(UDPServer.class.getName()).log(Level.SEVERE, null, ex);
-      }
+               
+        
   }
   
   public static String ConvertToUpper(String word)
