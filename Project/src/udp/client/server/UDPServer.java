22f38c387ce4c1d47210ebbb4a547a466e8babfb
WaleedYaser <waleedyaser95@gmail.com>
2017-03-21 01:22:28 +0200

    feat: add processing word feature

--- Project/src/udp/client/server/UDPServer.java
+++ Project/src/udp/client/server/UDPServer.java
@@ -5,38 +5,48 @@ import java.util.logging.Logger;
 
 
 public class UDPServer {
-  public static void main(String[] args) throws IOException{
+    
+    public static void main(String[] args) throws IOException {
            
-      try {
-          //  create the udp server socket with specific port number
-          DatagramSocket server=new DatagramSocket(2000);
+        try {
+            //  create the udp server socket with specific port number
+            DatagramSocket server=new DatagramSocket(2000);
            
-          // preparing the packet structure for recived packet 
+            // preparing the packet structure for recived packet 
           
-          byte[]receivedata=new byte[100];
-          DatagramPacket recievepacket=new DatagramPacket (receivedata,receivedata.length);
+            byte[]receivedata=new byte[100];
+            DatagramPacket recievepacket=new DatagramPacket (receivedata,
+                                                             receivedata.length);
           
-          
-              // waiting for incoming packet 
-              server.receive(recievepacket);
-              
-              // Extracting recieved packet 
-              String message =new String (recievepacket.getData());
-              System.out.println(message);
-              
-              // send response to the client 
+            while (true)
+            {
+                // waiting for incoming packet 
+                server.receive(recievepacket);
+
+                // Extracting recieved packet 
+                String message =new String (recievepacket.getData());
+                System.out.println(message);
+
+                // send response to the client 
                
-               String response="This is a greeting from the server " ;
-               byte[]senddata=response.getBytes();
-               InetAddress IPAddress=InetAddress.getByName("localhost");
-               DatagramPacket sendpacket=new DatagramPacket (senddata,senddata.length,IPAddress,9090);
-               server.send(sendpacket);
-          
+                String response = ConvertToUpper(message) ;
+                byte[]senddata=response.getBytes();
+                InetAddress IPAddress = InetAddress.getByName("localhost");
+                DatagramPacket sendpacket = new DatagramPacket (senddata, 
+                                                                senddata.length,
+                                                                IPAddress,
+                                                                9090);
+                server.send(sendpacket);
+            } 
                   
-              } catch (SocketException ex) {
-          Logger.getLogger(UDPServer.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SocketException ex) {
+        Logger.getLogger(UDPServer.class.getName()).log(Level.SEVERE, null, ex);
       }
-     
   }
-       
+  
+  public static String ConvertToUpper(String word)
+  {
+      return word.toUpperCase();
+  }
+  
 }
