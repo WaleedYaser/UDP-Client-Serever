8cf59282a01c512aa1fef1f5c7e18b4c65586292
WaleedYaser <waleedyaser95@gmail.com>
2017-03-21 01:37:09 +0200

    feat: send and recieve

--- Project/src/udp/client/server/UDPClient.java
+++ Project/src/udp/client/server/UDPClient.java
@@ -1,5 +1,6 @@
 import java.io.*;
 import java.net.*;
+import java.util.Scanner;
 
 public class UDPClient {
     public static void main(String[] args) throws Exception {
@@ -7,18 +8,33 @@ public class UDPClient {
         //create the client socket that used to contact to udp server 
         
         DatagramSocket clientsocket=new DatagramSocket(9090);
-        // sending to sever
-     
-        String message="Hello from client ";
-        byte[]senddata=message.getBytes();
-        InetAddress IPAddress=InetAddress.getByName("localhost");
-         DatagramPacket sendpacket=new DatagramPacket (senddata,senddata.length,IPAddress,2000);
-         clientsocket.send(sendpacket);
         
-         // receiving from server 
-           byte[]receivedata=new byte[100];
-          DatagramPacket recievepacket=new DatagramPacket (receivedata,receivedata.length);
-           clientsocket.receive(recievepacket);
+        
+        while (true)
+        {
+            // read user input
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Enter a word to capitalize: ");
+            String message = scanner.next();
+            
+            // sending to sever
+            byte[]senddata=message.getBytes();
+            InetAddress IPAddress=InetAddress.getByName("localhost");
+            DatagramPacket sendpacket = new DatagramPacket (senddata,
+                                                          senddata.length,
+                                                          IPAddress,
+                                                          2000);
+            clientsocket.send(sendpacket);
+
+            // receiving from server 
+            byte[]receivedata=new byte[100];
+            DatagramPacket recievepacket = new DatagramPacket (receivedata,
+                                                               receivedata.length);
+            clientsocket.receive(recievepacket);
+            String processedWord = new String(recievepacket.getData());
+            System.out.println("Processed: " + processedWord);
+            System.out.println("----------------------------");
+        }
                   
     }
    
